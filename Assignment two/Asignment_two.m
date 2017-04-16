1c2d155e104e44dced08f364f91c26615e950731
shenmemingzine <rongwingyu@gmail.com>
2017-04-16 15:39:55 -0700

    Update Asignment_two.m

--- Assignment two/Asignment_two.m
+++ Assignment two/Asignment_two.m
@@ -16,17 +16,17 @@ X_test = [ones(test_m, 1) Test_data(:, 1:256)];
 Y_test = Test_data(:, 257); 
 
 %% Problem One
-learning_rate = [0.1 1i^(1i) 0.5];
+learning_rate = 0.01;
 num_lr = length(learning_rate); % obtain the number of learning rate 
 
-% obtain the norm of each w and loss with different learning rate
+% obtain the norm of each w and lost with different learning rate
 w_norm = zeros(length(learning_rate), 1); 
 
 % initial optimal weight
-initial_w = rand(size(X_train, 2), 1);
+initial_w = zeros(size(X_train, 2), 1);
 
 for r = 1:num_lr
-    [w, l]= Batgrad(X_train, Y_train, 1000, initial_w, learning_rate(r));
+    w = Batgrad(X_train, Y_train, 1000, initial_w, learning_rate(r));
     w_norm(r) = norm(w, 2);
 end
 
@@ -38,3 +38,16 @@ ylabel('||w||')
 hold off
 
 %% Problem Two
+% Compute training accuracy
+pre_train = logclassify(sigmoid(X_train*w)); % Prediction on training data
+train_error = sum(abs(Y_train - pre_train));
+train_accuracy = train_error/train_m;
+
+% Compute testing accuracy
+pre_test = logclassify(sigmoid(X_test*w)); % Prediction on testing data
+test_error = sum(abs(Y_test - pre_test));
+test_accuracy = test_error/test_m;
+
+%% Problem Three
+
+%% Problem Four
