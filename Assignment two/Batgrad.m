151da4482f42127de70f9c1291a2338d77e192df
shenmemingzine <rongwingyu@gmail.com>
2017-04-17 19:31:22 -0700

    Update Batgrad.m

--- Assignment two/Batgrad.m
+++ Assignment two/Batgrad.m
@@ -1,35 +1,23 @@
-function w = Batgrad(X, Y, N, w0, r)
+function w = Batgrad(X, Y, N, w0, learning_rate, lambda)
 % Apply Batch gradient decent
 %
 % In N interation, if the lost is less than the setting threshold
-% stop computing the optimal weight;
-% if the lost is still greater than the setting threshold,
-% set the last w as optimal weight.
+% stop computing the optimal weight.
 
 % initialize optimal weight vector
 [samples, features] = size(X);
 w = w0; %obtain initial w
-
 wNorm = zeros(N, 1); % store the norm of weight in each interation
-
-% lost differences
-dif_loss = 0;
 
 for iter = 1:N
     delta = zeros(features, 1); % initialize error
     
-    for n = 1:samples
+    for n = 1:(samples*(2/3))
         h = sigmoid(X(n, :)*w); % hypothese function  
-        delta = delta + (Y(n) - h)*X(n, :)';
+        delta = delta + (h - Y(n))*X(n, :)';
     end
-    w = w + r*delta; % update optimal weight vector
-    wNorm(iter) = norm(w, 2);
-    loss = LossFunc(X, Y, w); % apply the new w to compute the lost
-        
-    if abs(loss - dif_loss)/loss <= 0.0001 % threshold 
-        break;
-    end
-    dif_loss = loss;    
+    w = w - learning_rate*(delta + lambda*sqrt(w'*w)); % update optimal weight vector
+    wNorm(iter) = norm(w, 2); 
 end
 
 figure
