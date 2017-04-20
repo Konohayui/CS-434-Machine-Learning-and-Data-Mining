eb32053934d1ab6be0c0986050cebe5a1d857ed1
shenmemingzine <rongwingyu@gmail.com>
2017-04-20 13:45:33 -0700

    Update Asignment_two.m

--- Assignment two/Asignment_two.m
+++ Assignment two/Asignment_two.m
@@ -16,12 +16,11 @@ X_test = [ones(Test_samples, 1) Test_data(:, 1:256)];
 Y_test = Test_data(:, 257); 
 
 %% Problem One
-learning_rate = (7:0.001:7.18)*1e-8;
+learning_rate = (1:0.1:15)*1e-8;
 num_lr = length(learning_rate); % obtain the number of learning rate 
 
-% obtain the norm of each w and loss with different learning rate
-w_norm = zeros(length(learning_rate), 1); 
-loss = w_norm;
+% obtain the loss with different learning rate
+loss = zeros(length(learning_rate), 1); 
 
 % initial optimal weight
 initial_w = zeros(size(X_train, 2), 1);
@@ -31,13 +30,13 @@ pre_loss = 0;
 
 for r = 1:num_lr
     w = Batgrad(X_train, Y_train, 100, initial_w, learning_rate(r), 0);
-    w_norm(r) = sqrt(w'*w);
     loss(r) = LossFunc(X_train, Y_train, w);
     if abs(loss(r) - pre_loss) < 1e-3 
         break
     end
-    pre_loss = loss(r);
 end
+
+hold off
 
 figure
 plot(learning_rate, loss, 'o-');
@@ -46,11 +45,7 @@ xlabel('Learning Rate')
 ylabel('Loss')
 hold off
 
-% r = 7.16e-8 is our the desire learning rate. 
-% If we pick r greater than 7.16e-8, our prediction will always one; 
-% if we pick r somewhat less than 7.16e-8, 
-% our prediction will always or get close to zero. In each case, our loss 
-% will be not a number (NaN) or close to positive/negative infinity
+% r = 1e-7 is our the desire learning rate. 
 
 %% Problem Two
 Iterations = [100 200 300 400 500];
@@ -61,7 +56,7 @@ train_accuracy = zeros(num_ite, 1);
 test_accuracy = train_accuracy;
 
 for i = 1:num_ite
-    train_w = Batgrad(X_train, Y_train, Iterations(i), initial_w, 7.16e-8, 0);
+    train_w = Batgrad(X_train, Y_train, Iterations(i), initial_w, 1e-7, 0);
     
     % Compute training accuracy
     train_pre = logclassify(sigmoid(X_train*train_w)); % Prediction on training data
@@ -91,7 +86,7 @@ hold off
 %
 
 %% Problem Four
-Lambda = [10e-3 10e-2 10e-1 1 10e1 10e2 10e3];
+Lambda = 10.^(-10:2:10);
 Num_lam = length(Lambda);
 
 % store training accuracy and testing accuracy with regularization term
@@ -99,7 +94,7 @@ re_train_accuracy = zeros(Num_lam, 1);
 re_test_accuracy = re_train_accuracy;
 
 for k = 1:Num_lam
-    re_train_w = Batgrad(X_train, Y_train, 200, initial_w, 7.16e-8, Lambda(k));
+    re_train_w = Batgrad(X_train, Y_train, 200, initial_w, 1e-7, Lambda(k));
     
     % Compute training accuracy
     re_train_pre = logclassify(sigmoid(X_train*re_train_w)); % Prediction on training data
