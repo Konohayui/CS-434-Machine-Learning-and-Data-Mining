eaf0f70654202bb73700ff57001e0757e1b7eae2
shenmemingzine <rongwingyu@gmail.com>
2017-05-11 10:00:54 -0700

    Update Gaussian_Kmeans.m

--- Gaussian_Kmeans.m
+++ Gaussian_Kmeans.m
@@ -1,14 +1,18 @@
+close all;
+
 sigma = eye(2,2);
 mu = [2, 3];
 r = mvnrnd(mu, sigma, 10);
 m1 = size(r, 1);
-figure
+leg1 = num2cell(r);
+figure  
 hold on
-plot(r(:,1), r(:,2), '+')
-
 for c = 1:m1
   ncr = mvnrnd([r(c,1), r(c,2)], sigma/5, 10);
   plot(ncr(:,1), ncr(:,2), '*')
+  text(r(c,1), r(c,2), num2str(c))
 end
-legend(r, 'betty')
+legend(strread(num2str(1:10),'%s'))
+plot(r(:,1), r(:,2), '+')
+
 hold off
