c40e09e530e4458d20dd7f6d5d542f38c2f758c7
shenmemingzine <rongwingyu@gmail.com>
2017-05-14 17:50:33 -0700

    Add files via upload

--- Assignment Four/ReadImage.m
+++ Assignment Four/ReadImage.m
@@ -1,9 +1,11 @@
 function image = ReadImage(group, X, k)
-Img = [];
+Img = zeros(28, 560);
 pos = find(group == k);
 ind = randperm(length(pos), 20);
-for l = ind
-    Img = [Img reshape(X(pos(l), :), 28, 28)];
+L = length(ind);
+
+for l = 1:L    
+    Img(:, (28*(l-1) + 1):(28*l)) = reshape(X(pos(ind(l)), :), 28, 28);
 end
 
 image = imshow(Img);
