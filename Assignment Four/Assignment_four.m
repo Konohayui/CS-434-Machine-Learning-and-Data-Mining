1a0b2e7125d2174819ce668ffac5112cfe54b321
shenmemingzine <rongwingyu@gmail.com>
2017-05-28 13:07:49 -0700

    Add files via upload

--- Assignment Four/Assignment_four.m
+++ Assignment Four/Assignment_four.m
@@ -4,20 +4,20 @@ clc; close all; clear all;
 X = load('data-2.txt');
 
 %% K-means Problem One
-K = 5;
-[group, sse]= Kmeans(X, K);
+K = 4;
+[group, sse] = Kmeans(X, K);
 iter = length(sse);
 
 figure
 plot(1:iter, sse, '-o')
 xlabel('iteration')
 ylabel('sse')
-title(['K-Means Clusering with k = ', num2str(K)])
+title(['K-Means Clusering with K = ', num2str(K)])
 
 %% Read Image
 for k = 1:K
     figure
-    image = ReadImage(group, X, k);
+    image = ReadImage(group, X, k, 30);
 end
 
 %% K-means Problem Two
@@ -29,24 +29,23 @@ SSE = zeros(L, 1);
 for k = 1:L
     minerr = zeros(iteration, 1);
     for itr = 1:iteration
-        [G, S] = Kmeans(X, K2(k));
+        [~, S] = Kmeans(X, K2(k));
         minerr(itr) = S(end);
     end
     SSE(k) = min(minerr);
 end
 
 figure
-plot(K2, SSE)
+plot(2:10, SSE, '-o')
 xlabel('K')
 ylabel('SSE')
-title('SSE with K = 2 to 10')
+title('K-Means Clusering with K = 2 to 10')
 
 %% Single Link
 [clust, infoM] = SingleL(X);
 clust = cell2mat(clust);
-clust = [clust; infoM(2,1) infoM(3,1), infoM(3,2)];
 figure
 dendrogram(clust)
 
 %% Complete Link
-% same
+% Same 
--- Assignment Four/SingleL.m
+++ Assignment Four/SingleL.m
@@ -5,58 +5,54 @@ function [clust, infoMatrix] = SingleL(X)
 % http://people.revoledu.com/kardi/tutorial/Clustering/Online-Hierarchical-Clustering.html
 
 samples = size(X, 1);
-clust = []; % initializing cluster
-c = samples; % initializing new number cluster
-distance = pdist(X); % compute pair wise distance
-DistValue = squareform(distance); % comstruct a matrix for each data point
+clust = [];
+c = samples; 
+distance = pdist(X);
+DistValue = squareform(distance);
+sortDist = sort(distance);
 DistMatrix = DistValue;
+ite = 1;
 
-% comstruct a matrix to store all information
-infoMatrix = zeros(samples + 1, samples + 1); 
+infoMatrix = zeros(samples + 1, samples + 1);
 infoMatrix(1, :) = 0:samples;
 infoMatrix(2:end, 1) = 1:samples;
 infoMatrix(2:end, 2:end) = DistMatrix;
 
 while true
-    [minD, pos]= min(distance); % find the minimum distance
-    DistN = DistMatrix; % rename distance matrix
-    DistN(triu(true(size(DistN)))) = NaN; % avoid duplicate distance
-    % find the index of the min distance
-    [min_i, min_j] = find(DistN == minD,1); 
-%     fprintf(['Min distance between ',num2str(min_i),' and ',...
-%         num2str(min_j), ' is ', num2str(minD),'\n'])
+    minD = sortDist(ite);
+    DistN = DistMatrix;
+    DistN(triu(true(size(DistN)))) = NaN;
+    [min_i, min_j] = find(DistN == minD,1);
     
-    if size(min_i) ~= 0 % to make sure obtain index of min distance 
-        c = c + 1; % update cluster
-        % append new cluster
-        clust = [clust; {infoMatrix(1, min_i + 1), ...
+    if size(min_i) ~= 0
+        fprintf(['Min distance between ',num2str(min_i),' and ',...
+            num2str(min_j), ' is ', num2str(minD),'\n'])
+        
+        c = c + 1;
+        clust = [clust; {infoMatrix(1, min_i + 1),...
             infoMatrix(1, min_j + 1), minD}]; 
-        % update cluster number to infoM
         infoMatrix(1, min_j + 1) = c;
         infoMatrix(min_j + 1, 1) = c;
         % Complete link
         % max_dist = max(DistM(:,min_i), DistM(:,min_j));
         min_dist = min(DistMatrix(:,min_i), DistMatrix(:,min_j));
         min_dist(min_j) = 0;
-        % update new distance to cluster
         DistMatrix(:,min_j) = min_dist;
         DistMatrix(min_j,:) = min_dist';
     end
     
-    % delete old distance
     DistMatrix(min_i,:) = [];
     DistMatrix(:,min_i) = [];
     infoMatrix(min_i + 1,:) = [];
     infoMatrix(:,min_i + 1) = [];
-    distance(pos) = [];
     infoMatrix(2:end, 2:end) = DistMatrix;
-%     display(infoMatrix)
     
-    [m, n] = size(DistMatrix);
+    [m,n] = size(infoMatrix);
     if m == 2 && n == 2
         break;
     end
-    
+    display(infoMatrix)
+    ite = ite + 1;
 end
 
 end
